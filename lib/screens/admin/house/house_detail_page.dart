785be405761d695531f66c603552c329d89c337d
KhoaDauTay <cuonglieu3256@gmail.com>
2021-10-06 02:42:12 +0700

    feat(mobile): finish app

--- lib/screens/admin/house/house_detail_page.dart
+++ lib/screens/admin/house/house_detail_page.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'package:mobile_final/constants/colors.dart';
+import 'package:mobile_final/constants.dart';
+import 'package:mobile_final/models/house_model.dart';
 import 'package:mobile_final/models/place_model.dart';
 
-class PlaceDetail extends StatelessWidget {
-  final PlaceModel placeModel;
-
-  PlaceDetail({required this.placeModel});
+class HouseDetailPage extends StatefulWidget {
+  final House house;
+  final PlaceModel place = placeCollection[0];
+  HouseDetailPage({required this.house});
 
   @override
+  State<HouseDetailPage> createState() => _HouseDetailPageState();
+}
+
+class _HouseDetailPageState extends State<HouseDetailPage> {
+  @override
   Widget build(BuildContext context) {
+    final height =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    final width = MediaQuery.of(context).size.width -
+        MediaQuery.of(context).padding.left -
+        MediaQuery.of(context).padding.right;
+    final space = height > 650 ? kSpaceM : kSpaceS;
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
+          padding: const EdgeInsets.symmetric(
+              horizontal: kPaddingM, vertical: kPaddingM - 2.0),
           child: ListView(
             children: [
               Hero(
-                tag: placeModel.title,
+                tag: widget.house.name,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(24),
-                  child: Image(
-                    fit: BoxFit.cover,
-                    image: AssetImage(placeModel.imagePath),
+                  child: Container(
+                    width: width,
+                    height: height / 4,
+                    child: Image(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(widget.house.image),
+                    ),
                   ),
                 ),
               ),
               SizedBox(
-                height: 30,
+                height: space,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -37,7 +54,7 @@ class PlaceDetail extends StatelessWidget {
                   Flexible(
                       flex: 3,
                       child: Text(
-                        placeModel.title,
+                        widget.house.name,
                         style: textTheme.headline4!.apply(color: Colors.black),
                       )),
                   Flexible(
@@ -46,7 +63,7 @@ class PlaceDetail extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
                             Text(
-                              "\$ ${placeModel.rent}",
+                              "\$ ${widget.house.rent}",
                               style: textTheme.headline5,
                             ),
                             Text(
@@ -57,12 +74,12 @@ class PlaceDetail extends StatelessWidget {
                 ],
               ),
               SizedBox(
-                height: 20,
+                height: space,
               ),
               Row(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
+                    padding: const EdgeInsets.only(right: kPaddingS),
                     child: Container(
                       height: 30,
                       width: 60,
@@ -83,7 +100,7 @@ class PlaceDetail extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
+                    padding: const EdgeInsets.only(right: kPaddingS),
                     child: Container(
                       height: 30,
                       width: 60,
@@ -104,7 +121,7 @@ class PlaceDetail extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
+                    padding: const EdgeInsets.only(right: kPaddingS),
                     child: Container(
                       height: 30,
                       width: 60,
@@ -127,7 +144,7 @@ class PlaceDetail extends StatelessWidget {
                 ],
               ),
               SizedBox(
-                height: 20,
+                height: space,
               ),
               Container(
                 decoration: BoxDecoration(
@@ -147,12 +164,12 @@ class PlaceDetail extends StatelessWidget {
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.all(kPaddingS),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              'Khoa',
+                              widget.house.reporter,
                               style: textTheme.caption,
                             ),
                             SizedBox(
@@ -180,21 +197,30 @@ class PlaceDetail extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: space,
+              ),
+              Text(
+                "Address: ${widget.house.address}",
+                style: textTheme.headline6,
+              ),
+              SizedBox(
+                height: space,
               ),
               Text(
                 "About",
                 style: textTheme.headline5,
               ),
               SizedBox(
-                height: 20,
+                height: space,
               ),
               Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis euismod magna ac ultricies. Nunc nec tristique tellus, non iaculis massa. Proin et eleifend mi, ac scelerisque ex. Vivamus eu ultricies enim. Suspendisse potenti. Aliquam orci libero, rhoncus molestie tellus a, scelerisque fringilla sem.",
+                widget.house.notes == null
+                    ? "Write your notes"
+                    : widget.house.notes,
                 style: textTheme.subtitle1!.apply(color: Colors.black45),
               ),
               SizedBox(
-                height: 20,
+                height: space,
               ),
               Text(
                 "Photos",
@@ -204,7 +230,7 @@ class PlaceDetail extends StatelessWidget {
                 height: 250,
                 child: ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    itemCount: placeModel.photoCollections.length,
+                    itemCount: widget.place.photoCollections.length,
                     itemBuilder: (BuildContext context, int index) {
                       return Padding(
                         padding: const EdgeInsets.all(8.0),
@@ -214,8 +240,8 @@ class PlaceDetail extends StatelessWidget {
                             height: 220,
                             width: 120,
                             fit: BoxFit.cover,
-                            image:
-                                AssetImage(placeModel.photoCollections[index]),
+                            image: AssetImage(
+                                widget.place.photoCollections[index]),
                           ),
                         ),
                       );
